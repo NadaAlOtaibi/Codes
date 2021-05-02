6cb5bae1eda1f0434891ed1fea47b1341a1f4759
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-05-02 21:05:44 +0300

    Create Project text generator.cs

---
+++ Project text generator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+
+namespace FileNada
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome to text generator :D!");
+            Console.WriteLine("Enter your name.");
+            string name = Console.ReadLine();
+            string History = "/Users/nadaalotaibi/Desktop/NadaFile/History.txt";
+            Console.WriteLine("Enter your file name.");
+            string Fname = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter("/Users/nadaalotaibi/Desktop/NadaFile/" + Fname + ".txt");
+            sw.WriteLine(name);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(History), true))
+            {
+                outputFile.WriteLine($" {name} created {Fname}.txt {DateTime.Now} \n");
+            }
+        
+            sw.Flush();
+
+            Console.WriteLine("Edit an existed File");
+            sw.WriteLine(Console.ReadLine());
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(History), true))
+            {
+                outputFile.WriteLine($" {name} edited {Fname}.txt {DateTime.Now} \n");
+            }
+            sw.Flush();
+            sw.Close();
+            Console.WriteLine("Delete file?y/n");
+            string ch = Console.ReadLine();
+            if (ch == "y" || ch == "Y")
+            {
+                if (File.Exists("/Users/nadaalotaibi/Desktop/NadaFile/" + Fname + ".txt"))
+                {
+                    File.Delete("/Users/nadaalotaibi/Desktop/NadaFile/" + Fname + ".txt");
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(History), true))
+                    {
+                        outputFile.WriteLine($" {name} deleted {Fname}.txt {DateTime.Now} \n");
+                    }
+
+
+                } }
+            }
+           
+
+        }
+    }
